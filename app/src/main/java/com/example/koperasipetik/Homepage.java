6442aff58b0a5173dd422aeeda7b35a68f506d7d
PeTIK <alfinadisurya@gmail.com>
2019-04-11 10:25:11 +0700

    commit input & history

--- app/src/main/java/com/example/koperasipetik/Homepage.java
+++ app/src/main/java/com/example/koperasipetik/Homepage.java
@@ -2,6 +2,7 @@ package com.example.koperasipetik;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class Homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_homepage);
+
+        (Button) btnInput = findViewById(R.id.btnInput);
+        (Button) btnHistory = findViewById(R.id.btnHistory);
     }
 }
